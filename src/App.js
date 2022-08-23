f5fa0e2598f82d9130a4b27a1edfd7182d6a232e
Zoryana Tur <106607876+ZoryanaTur@users.noreply.github.com>
2022-08-23 16:07:51 +0300

    Added Bottstrap and removed logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import Weather from "./Weather";
 import React from "react";
+
 import "./Styles.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Search App</h1>
-        <Weather />
-      </header>
-      <p className="open-source">
-        <a
-          href="https://github.com/ZoryanaTur/weather-app-react"
-          title="Check open-source code"
-        >
-          Open-source code
-        </a>{" "}
-        by Zoryana Tur
-      </p>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather Search App</h1>
+          <Weather />
+        </header>
+        <button btn btn-primary>
+          hello
+        </button>
+        <p className="open-source">
+          {" "}
+          This App was coded by Zoryana Tur and {""}{" "}
+          <a
+            href="https://github.com/ZoryanaTur/weather-app-react"
+            title="Check open-source code"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+        </p>
+      </div>
     </div>
   );
 }
